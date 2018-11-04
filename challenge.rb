8bfae18e4e15e02636a4c1707f6ad5b37281f5b1
Kristie Peyton <peytonkn@gmail.com>
2018-11-04 20:58:20 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -1,6 +1,6 @@
 #badge_maker
 name = ["Edsger", "Ada", "Charles","Alan","Grace","Linus","Matz"]
-num = ["1","2","3","4","5","6","7"]
+
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
@@ -11,7 +11,7 @@ name.each do |name|
 end
 
 #assign_rooms
-
+num = ["1","2","3","4","5","6","7"]
 def assign_rooms(name, num)
   puts "Hello,#{name}! You'll be assigned to room #{num}!"
 end
