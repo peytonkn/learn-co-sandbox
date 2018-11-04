4c40b7cf8e9cbeb472fac9620dc7497ada8baea8
Kristie Peyton <peytonkn@gmail.com>
2018-11-04 21:00:19 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -12,7 +12,7 @@ end
 
 #assign_rooms
 num = ["1","2","3","4","5","6","7"]
-def assign_rooms(name, num)
+def assign_rooms(num, name)
   puts "Hello,#{name}! You'll be assigned to room #{num}!"
 end
 name.each do |name, num|
