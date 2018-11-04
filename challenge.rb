7e9b975143be08ed1717bce657d202977a16e244
Kristie Peyton <peytonkn@gmail.com>
2018-11-04 20:36:14 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -1,8 +1,9 @@
+#badge_maker
 name = ["Edsger", "Ada", "Charles","Alan","Grace","Linus","Matz"]
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
-
+#batch_badge_creator
 name.each do |name|
   puts badge_maker(name)
 end
