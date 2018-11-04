21c09864ceb422897c5953f4317ccc57541752ed
Kristie Peyton <peytonkn@gmail.com>
2018-11-04 21:03:39 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -16,5 +16,5 @@ def assign_rooms(name, num)
   puts "Hello,#{name}! You'll be assigned to room #{num}!"
 end
 name.each do |name, num|
-  puts assign_rooms(name, num)
+  puts assign_rooms(name, (num))
 end
